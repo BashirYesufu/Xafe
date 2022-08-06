b4f27ac139d52bf761bec9572c64811550efd7e1
bash <yesufubashir@gmail.com>
2022-08-06 13:17:51 +0100

    set up firebase for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:xafe/route_handler.dart';
 import 'package:xafe/utilities/providers/provider_list.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const Xafe());
